d7eee7fa02d598feb98448c4e8c2bee723b1b89c
Shaylen Young <young777@fullsail.edu>
2013-06-13 21:33:12 -0500

    industry js

---
+++ expressions_industry/js/industry.js
@@ -0,0 +1,13 @@
+// Shaylen Young - 06 - 13 - Industry
+//Calculating my check along with stat bonus
+
+var hoursworked = 40;
+var perhour = 17;
+var stats = 5;
+var beforestat = perhour * hoursworked
+var statbonus = stats * hoursworked
+
+var answer;
+answer = beforestat + statbonus
+
+console.log(answer);
