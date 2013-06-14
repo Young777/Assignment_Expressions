98afa1212d90d12d9b8beb60e37e39b50d1b54ef
Shaylen Young <young777@fullsail.edu>
2013-06-13 21:34:47 -0500

    wacky js

---
+++ expressions_wacky/js/wacky.js
@@ -0,0 +1,7 @@
+//Shaylen Young - 06 - 13 - Wacky
+//How Many Scientist Does It Take To Screw In a Light Bulb
+var scientist = 1;
+var bulb = 1;
+// Ready For The Joke?
+var answer = "1, but it would have to change bwabwabwabwa!"
+console.log(answer)
